2ed5cda9e34a6e2228e47b5bf57d8842d2c7d318
mi4seven <mi4seven@gmail.com>
2020-12-03 05:53:37 +0200

    Minor fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     def account_update_params
-        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :curretn_password)
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
     end
 
 end
