6ac834ea0414f70aa7e236864dcaa4e5d06c3ff2
mi4seven <mi4seven@gmail.com>
2020-12-06 23:20:24 +0200

    Hide has_one_attached for prod environment

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,5 +1,7 @@
 class UserProfile < ApplicationRecord
     belongs_to :user
-    has_one_attached :image
+    if ENV['RAILS_ENV'].to_s != 'production' 
+        has_one_attached :image
+    end    
     #validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]    
 end
