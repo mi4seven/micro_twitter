5d758dc09e761a33a5e4313876fc11d9271e6e92
mi4seven <mi4seven@gmail.com>
2020-12-06 23:14:23 +0200

    Add checking Environment

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,5 +1,5 @@
 class UserProfile < ApplicationRecord
     belongs_to :user
-    #has_one_attached :image
+    has_one_attached :image
     #validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]    
 end
