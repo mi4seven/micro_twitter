3fbb3fd959ebcc0f942745770aea9a72dd174667
mi4seven <mi4seven@gmail.com>
2020-12-13 01:50:19 +0200

    Added Who to Follow list

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
     
   has_one :user_profile, :dependent => :destroy 
-  has_many :tweeets
+  has_many :tweeets, :dependent => :destroy
 
   has_many :followers
   has_many :followings, foreign_key: 'follower_id', class_name: 'Follower'
 
-  # def build_user_profile
-  #   UserProfile.create(user: self) 
-  # end
-
+  def who_to_follow
+    User.where.not(:id => 1).where.not(:id => Follower.where(:follower_id => 1).select(:user_id))
+  end
 
 end
